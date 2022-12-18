5855e135525cf26eb211e554d4ecbc015719e734
isamd4 <isamuhammadhassan@gmail.com>
2022-12-18 02:22:26 +0100

    initializing my embedded system repo

---
+++ part1/final_project/sources.mk
@@ -0,0 +1,44 @@
+#******************************************************************************
+# Copyright (C) 2017 by Alex Fosdick - University of Colorado
+#
+# Redistribution, modification or use of this software in source or binary
+# forms is permitted as long as the files maintain this copyright. Users are 
+# permitted to modify this and use it to learn about the field of embedded
+# software. Alex Fosdick and the University of Colorado are not liable for any
+# misuse of this material. 
+#
+#*****************************************************************************
+SOURCES = ./src/main.c\
+		  ./src/memory.c\
+		  ./src/interrupts_msp432p401r_gcc.c\
+		  ./src/startup_msp432p401r_gcc.c\
+		  ./src/system_msp432p401r.c
+PLATFORM=
+# Add your Source files to this variable
+ifeq ($(PLATFORM),MSP432)
+SOURCES = ./src/main.c\
+		  ./src/memory.c\
+		  ./src/interrupts_msp432p401r_gcc.c\
+		  ./src/startup_msp432p401r_gcc.c\
+		  ./src/system_msp432p401r.c
+
+# Add your include paths to this variable
+INCLUDES = -I ./src\
+		   -I ./include/common\
+		   -I ./include/msp432\
+		   -I ./include/CMSIS
+
+else ifeq($(PLATFORM),HOST)
+SOURCES = ./src/main.c\
+		  ./src/memory.c\
+		  ./src/data.c\
+		  ./src/course1.c\
+		  ./src/stats.c
+
+# Add your include paths to this variable
+INCLUDES = -I ./src\
+		   -I ./include/common\
+		   -I ./include/msp432\
+		   -I ./include/CMSIS
+
+endif
---
+++ part1/week2/README.md
@@ -0,0 +1,1 @@
+NAME= Muhammad Isa Hassan
